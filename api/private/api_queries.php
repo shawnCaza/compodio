3604251b74e1ea31bca79bee6e8f545897a47c20
shawnCaza <theshawn@gmail.com>
2022-12-23 01:15:40 -0500

    added tags to get_all_shows api

--- api/private/api_queries.php
+++ api/private/api_queries.php
@@ -4,8 +4,15 @@ function get_all_shows(){
 
     global $db;
     $sql = "
-    select `shows`.*, `eps`.`mp3`, `eps`.`newestEpDate` from `shows`
-    inner join (
+        select `shows`.*, `eps`.`mp3`, `eps`.`newestEpDate`, `show_tags`.`tags` from `shows`
+        
+        left join (
+                SELECT show_id, JSON_ARRAYAGG(tag_id) AS tags
+                FROM show_tags GROUP BY show_id
+        ) show_tags
+        on `show_tags`.`show_id` = `shows`.`id`
+            
+        inner join (
             SELECT show_id, mp3, ep_date newestEpDate
             from `episodes` eps
             where id in (
@@ -13,8 +20,9 @@ function get_all_shows(){
                 from episodes
                 group by show_id
             )
-    ) eps 
-    on `shows`.`id` = `eps`.`show_id`;
+        ) eps 
+        on `shows`.`id` = `eps`.`show_id`
+        ;
     ";
     $result = mysqli_query($db, $sql);
     confirm_result_set($result);
