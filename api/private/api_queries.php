ff1c6642236841c217a9f39a2a01585e2afabb0b
shawnCaza <theshawn@gmail.com>
2022-12-23 01:27:02 -0500

    get all show comments

--- api/private/api_queries.php
+++ api/private/api_queries.php
@@ -6,13 +6,15 @@ function get_all_shows(){
     $sql = "
         SELECT `shows`.*, `eps`.`mp3`, `eps`.`newestEpDate`, `show_tags`.`tags` 
         FROM `shows`
-        
+
+        /* Collect all the show tags into an array */
         LEFT JOIN (
                 SELECT show_id, JSON_ARRAYAGG(tag_id) AS tags
                 FROM show_tags GROUP BY show_id
         ) show_tags
         ON `show_tags`.`show_id` = `shows`.`id`
-            
+        
+        /* Join most recent episode */
         INNER JOIN (
             SELECT show_id, mp3, ep_date newestEpDate
             FROM `episodes` eps
