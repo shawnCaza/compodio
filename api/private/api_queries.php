2a472174836b127d16447348e0be0cde96010a22
shawnCaza <theshawn@gmail.com>
2022-12-23 01:20:44 -0500

    sql case fix

--- api/private/api_queries.php
+++ api/private/api_queries.php
@@ -4,24 +4,25 @@ function get_all_shows(){
 
     global $db;
     $sql = "
-        select `shows`.*, `eps`.`mp3`, `eps`.`newestEpDate`, `show_tags`.`tags` from `shows`
+        SELECT `shows`.*, `eps`.`mp3`, `eps`.`newestEpDate`, `show_tags`.`tags` 
+        FROM `shows`
         
-        left join (
+        LEFT JOIN (
                 SELECT show_id, JSON_ARRAYAGG(tag_id) AS tags
                 FROM show_tags GROUP BY show_id
         ) show_tags
-        on `show_tags`.`show_id` = `shows`.`id`
+        ON `show_tags`.`show_id` = `shows`.`id`
             
-        inner join (
+        INNER JOIN (
             SELECT show_id, mp3, ep_date newestEpDate
-            from `episodes` eps
-            where id in (
-                select max(id) id
-                from episodes
-                group by show_id
+            FROM `episodes` eps
+            WHERE id IN (
+                SELECT MAX(id) id
+                FROM episodes
+                GROUP BY show_id
             )
         ) eps 
-        on `shows`.`id` = `eps`.`show_id`
+        ON `shows`.`id` = `eps`.`show_id`
         ;
     ";
     $result = mysqli_query($db, $sql);
