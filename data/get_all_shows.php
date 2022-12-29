31b8800ff92a0abbe5ed0ff3cb01458a6596f693
shawnCaza <theshawn@gmail.com>
2022-12-29 01:53:37 -0500

    nextJS setup and shows query call

--- data/get_all_shows.php
+++ data/get_all_shows.php
@@ -1,5 +1,6 @@
 <?php
 require_once('private/initialize.php');
 require_once(PRIVATE_PATH .'/api_queries.php');
+header('Access-Control-Allow-Origin: http://localhost:3000', false); //TODO only for dev env
 $result = get_all_shows();
 echo json_encode($result->fetch_all(MYSQLI_ASSOC));
---
+++ ui/pages/_app.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import { getShows, showsStaleTime } from "../hooks/queries/shows";
+
+function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+  queryClient.setQueryDefaults('shows', { queryFn: getShows, staleTime: showsStaleTime()})
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
