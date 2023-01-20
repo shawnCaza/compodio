3fd2f8e348e205b8d2973f23a2520a4b8b5723cc
shawnCaza <theshawn@gmail.com>
2023-01-19 23:10:07 -0500

    return freq of dom img colour + unused experiments

--- scraper/radio_scrape/radio_scrape/image_colour.py
+++ scraper/radio_scrape/radio_scrape/image_colour.py
@@ -1,9 +1,30 @@
+from decimal import *
+
 import cv2, numpy as np
+from PIL import Image, ImageChops
 import colorsys
 from sklearn.cluster import KMeans
 
-# finding dominant colour in image
+# finding avg dominant colours in image
 # from https://stackoverflow.com/questions/43111029/how-to-find-the-average-colour-of-an-image-in-python-with-opencv
+
+# finding border colour from https://stackoverflow.com/questions/10985550/detect-if-an-image-has-a-border-programmatically-return-boolean
+
+def is_there_a_border(image_path):
+    with Image.open(image_path).convert('RGB') as im:
+
+        corner_pixel = im.getpixel((0,0))
+
+        bg = Image.new(im.mode, im.size, corner_pixel)
+        diff = ImageChops.difference(im, bg)
+        diff = ImageChops.add(diff, diff, 2.0, -100)
+        bbox = diff.getbbox()
+        corner_rgb = [value for value in corner_pixel]
+        print(corner_rgb)
+        corner_hex = convert_to_hex([value for value in corner_rgb])
+        
+    return all((bbox[0], bbox[1], (bbox[0] + bbox[2]) <= im.size[0], (bbox[1] + bbox[3]) <= im.size[1])), corner_hex
+
 
 def get_colour_frequencies(cluster, centroids):
     """Returns tuple containing (frequency, [r,g,b])"""
@@ -14,24 +35,30 @@ def get_colour_frequencies(cluster, centroids):
     hist = hist.astype("float")
     hist /= hist.sum()
 
-    # Group cluster's color and percentage
-    colours = [(percent, color) for (percent, color) in zip(hist, centroids)]
+    # Group cluster's percentage, rgb. hex
+    colours = [(percent, color, convert_to_hex(color)) for (percent, color) in zip(hist, centroids)]
     return colours
 
-def convert_to_hex(colours):
-    """Converts RGB to hex"""
-    for colour in colours:
-        rounded_rgb = [int(x) for x in colour[1]]
-        hex = "{:02x}{:02x}{:02x}".format(*rounded_rgb)
-        yield hex
+def convert_to_hex(rgb_colour_value):
+    """
+        Converts a list of RGB values to a hex value. 
+     
+    """
+    rounded_rgb = [int(x) for x in rgb_colour_value]
+    hex = "{:02x}{:02x}{:02x}".format(*rounded_rgb)
+    return hex
 
-def find_dominant_colours(image_path, quantity = 3):
+def find_avg_dominant_colours(image_path, quantity = 3):
     """
         Returns array of dominant colours in an Image sorted from lightest to darkest.
         `image_path` specifies the path to a local image.
         `quantity` specifies the number of colours to return.
         Within the `get_colour_frequencies` function, the frequency of each colour is calulated, but this is not returned by `find_dominant_colours` at present.
     """
+
+    # Doesn't always seem to work. When it does, corner image might not be most representative pixel. I some cases images need to be trimmed.
+    # has_solid_border, potential_border_hex_colour = is_there_a_border(image_path)
+    # print('solid border?', has_solid_border, potential_border_hex_colour)
 
     # Load image and convert to a list of pixels
     image = cv2.imread(image_path)
@@ -40,17 +67,26 @@ def find_dominant_colours(image_path, quantity = 3):
 
     # Find and display most dominant colors
     cluster = KMeans(n_clusters=quantity).fit(reshape)
-    colours = get_colour_frequencies(cluster, cluster.cluster_centers_)
+    freq_n_colours = get_colour_frequencies(cluster, cluster.cluster_centers_)
 
     # Sort from lightest to darkest 
-    colours.sort(reverse=True, key=lambda colour: colorsys.rgb_to_hls(*colour[1])[1])
+    freq_n_colours.sort(reverse=True, key=lambda colour: colorsys.rgb_to_hls(*colour[1])[1])
 
-    dominant_hex_colours = list(convert_to_hex(colours))
+    # will just save frequency data for now so we have more options later.
+    # if one colour is super dominant, just use that one.
+    # sum_of_frequencies = sum([freq_colour[0] for freq_colour in freq_n_colours])
+    # super_dom_colours = [freq_colour[2] for freq_colour in freq_n_colours if freq_colour[0]/sum_of_frequencies > .7]
+    
+    # if len(super_dom_colour):
 
-    return dominant_hex_colours
+        
+    freq_n_hex = [{'freq': round(Decimal(freq_hex[0]),2), 'hex': freq_hex[2]} for freq_hex in freq_n_colours]
+
+
+    return freq_n_hex
 
 
 if __name__ == '__main__':
 
-    dom_colours = find_dominant_colours('/Users/scaza/Desktop/DJ-medicineman-CIUT-Show-Page.jpeg')
+    dom_colours = find_avg_dominant_colours('/Users/scaza/Desktop/ciut-beyond-the-headlines_250.jpeg')
     print(dom_colours)
