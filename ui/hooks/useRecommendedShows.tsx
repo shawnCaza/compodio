df45c5026a76c2ff3f4533caf84663e37ede203f
Shawn Caza <theshawn@gmail.com>
2023-09-23 23:32:00 -0400

    using Durstenfeld shuffle for recommended shows

--- ui/hooks/useRecommendedShows.tsx
+++ ui/hooks/useRecommendedShows.tsx
@@ -1,5 +1,5 @@
 import { useShowsQuery } from '../hooks/queries/shows';
-
+import { shuffleArray } from '../functions/utility/shuffleArray';
 import { Show } from '../hooks/queries/shows'
 
 interface randomShowResults{
@@ -11,14 +11,12 @@ export type {randomShowResults}
 export function useRecommendedShows() {
     const shows = useShowsQuery();
 
-
     if(!shows || !shows.length){return {recShowsSuffled: undefined, serverRecShows: undefined}}
 
     const recIds = ['99', '146', '200', '83', '100', '112', '175', '164', '135', '206', '94', '219', '166']
 
-
     // select 10 random shows from the list of recommended shows in random order
-    const recShowsSuffled = shows.filter(show => {return recIds.includes(show.id)}).sort(() => Math.random() - 0.5).slice(0,10)
+    const recShowsSuffled = shuffleArray(shows.filter(show => {return recIds.slice(0,10).includes(show.id)}))
     
     const serverRecShows = shows.filter(show => {return recIds.slice(0,10).includes(show.id)})
 
