528cda94d5ca744d1991e4830ceaaccdda8e935d
Shawn Caza <theshawn@gmail.com>
2023-09-24 01:25:58 -0400

    fix: location of slice call on useRecommendedShows

--- ui/hooks/useRecommendedShows.tsx
+++ ui/hooks/useRecommendedShows.tsx
@@ -13,10 +13,10 @@ export function useRecommendedShows() {
 
     if(!shows || !shows.length){return {recShowsSuffled: undefined, serverRecShows: undefined}}
 
-    const recIds = ['99', '146', '200', '83', '100', '112', '175', '164', '135', '206', '94', '219', '166']
+    const recIds = ['99', '146', '200', '83', '100', '112', '175', '164', '135', '206', '94', '219', '166', '79']
 
     // select 10 random shows from the list of recommended shows in random order
-    const recShowsSuffled = shuffleArray(shows.filter(show => {return recIds.slice(0,10).includes(show.id)}))
+    const recShowsSuffled = shuffleArray(shows.filter(show => {return recIds.includes(show.id)})).slice(0,10)
     
     const serverRecShows = shows.filter(show => {return recIds.slice(0,10).includes(show.id)})
 
