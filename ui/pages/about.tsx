8ad683635ca01189310f24181eaa3990ef041008
Shawn Caza <theshawn@gmail.com>
2024-01-07 17:09:12 -0500

    bug fix: about page linting errors

--- ui/pages/about.tsx
+++ ui/pages/about.tsx
@@ -10,15 +10,15 @@ import ContentSection from "../components/layout/ContentSection/contentSection"
           <ContentSection heading="Simplifying Access to Community Radio Content" tag='h2' centered={false} readingWidth={true}>
             
             <p>
-              Dissapointed it wasn't possible to subscribe to the local community radio shows I listen to, I created this website as a solution to my frustration.
+              Dissapointed it wasn&apos;t possible to subscribe to the local community radio shows I listen to in podcast format, I created this website.
             </p>
             <p>
-              Please note, I just made this website for fun, and I'm not affiliated with any of the radio stations or shows listed here.
+              Please note, I just made this website for fun, and I&apos;m not affiliated with any of the radio stations or shows listed here.
             </p>
           </ContentSection>
           <ContentSection heading="Get involved" tag='h2' centered={false} readingWidth={true}>
             <p>
-              Compodio is an open source project. If you have ideas for improvements, or would like to contribute, please visit the <a href="https://github.com/shawnCaza/compodio" target='_blank' rel="noopener">GitHub repo</a>.
+              Compodio is an open source project. If you have ideas for improvements, or would like to contribute, please visit the <a href="https://github.com/shawnCaza/compodio" target='_blank' rel="noreferrer noopener">GitHub repo</a>.
             </p>
           </ContentSection>
         </ContentSection>
