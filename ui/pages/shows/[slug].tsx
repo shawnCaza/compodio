20dcfa14433bc9c74c305609fc70ec6738c5d944
DanieII <danielbogdanov1748@gmail.com>
2024-07-20 21:51:20 +0300

    Use FeedLinks component instead of RssLink component on show pages

--- ui/pages/shows/[slug].tsx
+++ ui/pages/shows/[slug].tsx
@@ -1,30 +1,29 @@
 import { useRouter } from 'next/router'
-import Link from 'next/link';
-import { dehydrate, QueryClient} from 'react-query';
-import { Server, Client } from "react-hydration-provider";
+import Link from 'next/link'
+import { dehydrate, QueryClient } from 'react-query'
+import { Server, Client } from 'react-hydration-provider'
 
-import {GoGlobe} from 'react-icons/go';
-import {CiClock2} from 'react-icons/ci';
-import {IoPeopleSharp} from 'react-icons/io5';
-import {IoTimeSharp} from 'react-icons/io5';
+import { GoGlobe } from 'react-icons/go'
+import { CiClock2 } from 'react-icons/ci'
+import { IoPeopleSharp } from 'react-icons/io5'
+import { IoTimeSharp } from 'react-icons/io5'
 
-import GradientBg from '../../components/commonElements/GradientBg';
-import ShowImgContainer from '../../components/show/showPage/ImgContainer/ShowImgContainer';
-import RssLink from '../../components/show/podcastFeedLinks/rssLink/RssLink';
-import EpDate from '../../components/show/epDate/EpDate';
-import { useShowLength } from '../../components/show/hooks/useShowLength';
-import { useRecommendedShows } from '../../hooks/useRecommendedShows';
-import ContentSection from '../../components/layout/ContentSection/contentSection';
-import CardCollection from '../../components/layout/cardElements/cardCollection/CardCollection';
-import CardContent from '../../components/show/showCards/cardContent/CardContent';
+import GradientBg from '../../components/commonElements/GradientBg'
+import ShowImgContainer from '../../components/show/showPage/ImgContainer/ShowImgContainer'
+import RssLink from '../../components/show/podcastFeedLinks/rssLink/RssLink'
+import EpDate from '../../components/show/epDate/EpDate'
+import { useShowLength } from '../../components/show/hooks/useShowLength'
+import { useRecommendedShows } from '../../hooks/useRecommendedShows'
+import ContentSection from '../../components/layout/ContentSection/contentSection'
+import CardCollection from '../../components/layout/cardElements/cardCollection/CardCollection'
+import CardContent from '../../components/show/showCards/cardContent/CardContent'
 
-import { useShowsQuery, Show } from '../../hooks/queries/shows';
-import { getTags } from '../../hooks/queries/tags';
-import TagsContainer from '../../components/layout/cardElements/TagsContainer/TagsContainer';
+import { useShowsQuery, Show } from '../../hooks/queries/shows'
+import { getTags } from '../../hooks/queries/tags'
+import TagsContainer from '../../components/layout/cardElements/TagsContainer/TagsContainer'
+import FeedLinks from '../../components/show/podcastFeedLinks/feedLinks/FeedLinks'
 
 import styles from './showsPage.module.scss'
-
-
 
 // export async function getServerSideProps() {
 //     const queryClient = new QueryClient();
@@ -33,7 +32,7 @@ import styles from './showsPage.module.scss'
 //       queryClient.prefetchQuery('tags', getTags),
 //       queryClient.prefetchQuery('shows', getShows)
 //     ]);
-  
+
 //     return {
 //       props: {
 //         dehydratedState: dehydrate(queryClient),
@@ -42,21 +41,30 @@ import styles from './showsPage.module.scss'
 //   }
 
 interface LinkIconProps {
-  icon: React.ReactNode;
-  label: string;
-  txt: string;
-  link?: string;
+  icon: React.ReactNode
+  label: string
+  txt: string
+  link?: string
 }
 
-function LinkIcon({icon, label, txt, link=undefined }:LinkIconProps){
+function LinkIcon({ icon, label, txt, link = undefined }: LinkIconProps) {
   return (
     <div className={styles.iconAndTextContainer}>
-      
-          <span className={styles.linkIcon} role='presentation'>{icon}</span>
-          <span className='screen-reader-text'>{label}:</span> 
-          {link && <a className={styles.iconTxt} href={link} target='_blank' rel="noreferrer">{txt}</a> }
-          {!link && <span className={styles.iconTxt}>{txt}</span>}
-         
+      <span className={styles.linkIcon} role='presentation'>
+        {icon}
+      </span>
+      <span className='screen-reader-text'>{label}:</span>
+      {link && (
+        <a
+          className={styles.iconTxt}
+          href={link}
+          target='_blank'
+          rel='noreferrer'
+        >
+          {txt}
+        </a>
+      )}
+      {!link && <span className={styles.iconTxt}>{txt}</span>}
     </div>
   )
 }
@@ -64,65 +72,87 @@ function LinkIcon({icon, label, txt, link=undefined }:LinkIconProps){
 export default function ShowPage() {
   const router = useRouter()
   const querySlug = router.query.slug as string
-  const shows = useShowsQuery();
-  const show = shows?.find(show => show.slug === querySlug);
-  const showLength = useShowLength(show?.duration);
+  const shows = useShowsQuery()
+  const show = shows?.find((show) => show.slug === querySlug)
+  const showLength = useShowLength(show?.duration)
   // wrap show.desc in paragraph tags if not already present
-  const htmlDesc = show?.desc && show.desc.match(/^<p>/) ? show.desc : `<p>${show?.desc}</p>`;
-  
-  const recShowsShuffled:Show[]|undefined = useRecommendedShows();
-  if (!show){
-    return null;
+  const htmlDesc =
+    show?.desc && show.desc.match(/^<p>/) ? show.desc : `<p>${show?.desc}</p>`
+
+  const recShowsShuffled: Show[] | undefined = useRecommendedShows()
+  if (!show) {
+    return null
   }
 
   return (
     <>
-    <div className={styles.detailAndImageWrapper}>
-
-      <div className={styles.showImgWrapper}>
+      <div className={styles.detailAndImageWrapper}>
+        <div className={styles.showImgWrapper}>
           <ShowImgContainer show={show} />
-      </div> 
-
-      <div className={styles.showDetails}>
-        
-        <h1>{show.showName}</h1>
-        
-        <ContentSection heading='Subscribe:' tag='h5' centered={true} spacing='tight'>
-            <RssLink showId={show.id} slug={show.slug} showName={show.showName} />
-        </ContentSection>
-        
-
-        {show.desc &&
-
-          <div className={styles.desc} dangerouslySetInnerHTML={{__html:htmlDesc}}  />
-        }
-        <div className={styles.iconDetailsList}>
-          {/* <LinkIcon icon={<IoCalendarClearSharp/>} label='Latest Episode' txt={<EpDate dtStr={show.newestEpDate}/>} /> */}
-          
-          
-          {show.host && <LinkIcon icon={<IoPeopleSharp/>} label='Host' txt={show.host} />}
-          
-          <LinkIcon icon={<GoGlobe/>} label='Website' txt={`${show.source.toUpperCase()}`} link={show.internal_link} />
-      
-          
-          {showLength && 
-          <LinkIcon icon={<IoTimeSharp/>} label={'Episode Length'} txt={showLength} />
-          }
         </div>
-        {show.tagIds &&
-          <TagsContainer currentTagIds={JSON.parse(show.tagIds)} maxTags={null} />
-        }
+
+        <div className={styles.showDetails}>
+          <h1>{show.showName}</h1>
+
+          <ContentSection
+            heading='Subscribe:'
+            tag='h5'
+            centered={true}
+            spacing='tight'
+          >
+            <FeedLinks
+              showId={show.id}
+              slug={show.slug}
+              showName={show.showName}
+              extFeeds={show.extFeeds}
+            />
+          </ContentSection>
+
+          {show.desc && (
+            <div
+              className={styles.desc}
+              dangerouslySetInnerHTML={{ __html: htmlDesc }}
+            />
+          )}
+          <div className={styles.iconDetailsList}>
+            {/* <LinkIcon icon={<IoCalendarClearSharp/>} label='Latest Episode' txt={<EpDate dtStr={show.newestEpDate}/>} /> */}
+
+            {show.host && (
+              <LinkIcon icon={<IoPeopleSharp />} label='Host' txt={show.host} />
+            )}
+
+            <LinkIcon
+              icon={<GoGlobe />}
+              label='Website'
+              txt={`${show.source.toUpperCase()}`}
+              link={show.internal_link}
+            />
+
+            {showLength && (
+              <LinkIcon
+                icon={<IoTimeSharp />}
+                label={'Episode Length'}
+                txt={showLength}
+              />
+            )}
+          </div>
+          {show.tagIds && (
+            <TagsContainer
+              currentTagIds={JSON.parse(show.tagIds)}
+              maxTags={null}
+            />
+          )}
+        </div>
       </div>
-
-
-
-    </div>
-    <ContentSection heading='Recommended' tag='h2'>
-
-        {recShowsShuffled &&
-          <CardCollection cardDataCollection={recShowsShuffled} CardContent={CardContent} singleRow={true} />         
-        }
-    </ContentSection>
+      <ContentSection heading='Recommended' tag='h2'>
+        {recShowsShuffled && (
+          <CardCollection
+            cardDataCollection={recShowsShuffled}
+            CardContent={CardContent}
+            singleRow={true}
+          />
+        )}
+      </ContentSection>
     </>
   )
 }
